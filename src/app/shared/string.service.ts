36acb5c7566b1079a7d77b79d2b9a3ec390f43dc
Jahan <34219176+JahanUoL@users.noreply.github.com>
2021-02-23 18:35:28 +0000

    prod build

--- src/app/shared/string.service.ts
+++ src/app/shared/string.service.ts
@@ -15,9 +15,7 @@ export class StringService {
   animationSpeed: number = 800;
 
   constructor() {
-        // [this.stack, this.needle, this.stackArr, this.needleArr] = ['123456789','123',[],[]];
-    [this.stack, this.needle, this.stackArr, this.needleArr] = ['ABCABCABC','ABC',[],[]];
-    // [this.stack, this.needle, this.stackArr, this.needleArr] = ['ABCDABCDABDE','ABCDABD',[],[]];
+    [this.stack, this.needle, this.stackArr, this.needleArr] = ['ABCDABCDABDE','ABCDABD',[],[]];
     this.updateStringsArray();
   }
 
